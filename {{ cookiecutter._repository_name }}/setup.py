fc5b332565735b2c2974cee2e9c8b5ddae00d3f0
Tim Thatcher <4790109+trthatcher@users.noreply.github.com>
2020-02-26 17:40:57 -0500

    Fix variable name

--- {{ cookiecutter._repository_name }}/setup.py
+++ {{ cookiecutter._repository_name }}/setup.py
@@ -7,7 +7,7 @@ setup(
     name='{{ cookiecutter.package_name }}',
     version=VERSION,
     description='{{ cookiecutter.project_name }}',
-    author='{{ cookiecutter.owner }}',
+    author='{{ cookiecutter.author_name }}',
     packages=find_packages('{{ cookiecutter.package_name }}'),{% if cookiecutter.license != 'None' %}
     license='{{ cookiecutter.license }}',{% endif %}
     python_requires='>=3.6'
