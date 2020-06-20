0ff0cd67e635db53ecef7b45042bd326ee585837
Tim Thatcher <4790109+trthatcher@users.noreply.github.com>
2020-06-20 14:20:32 -0400

    Update version details. Refactor ModelHandler as ModelManager

--- {{ cookiecutter.repository_name }}/setup.py
+++ {{ cookiecutter.repository_name }}/setup.py
@@ -10,5 +10,5 @@ setup(
     author='{{ cookiecutter.author_name }}',
     packages=find_packages('{{ cookiecutter.package_name }}'),
     {%- if cookiecutter.license != 'None' -%}license='{{ cookiecutter.license }}',{%- endif -%}
-    python_requires='>=3.6'
+    python_requires='>=3.7'
 )
--- {{ cookiecutter.repository_name }}/{{ cookiecutter.package_name }}/model.py
+++ {{ cookiecutter.repository_name }}/{{ cookiecutter.package_name }}/model.py
@@ -1,24 +1,92 @@
 '''Model definition, training and validation'''
 
+import pickle
 from . import home_dir
 from pathlib import Path
 
 
-class ModelHandler:
-    '''Class for working with models'''
+class ModelManager:
+    '''Machine learning model manager.
 
-    def __init__(self):
-        self.model = None
+    Attributes
+    ----------
+    model : object
+        The machine learning model object.
+    version : object
+        A string-serializable object to represent the version of the model
+        object.
+
+    Methods
+    -------
+    serialize(model_path)
+        Persist the current model to the file specified by model_path.
+    deserialize(model_path)
+        Deserialize a model from the file specified by model_path.
+    fit(y, X)
+        Fit a model to target vector y with respect to data matrix X.
+    predict(X)
+        Predict values for y with respect to data matrix X.
+    '''
+
+    def __init__(self, model=None, version=1):
+        self.model = model
+        self.version = version
+
+    @property
+    def version(self):
+        return self._version
+
+    @version.setter
+    def version(self, version_value):
+        # Enforce version invariants here
+        self._version = version_value
+
+    def serialize(self, model_path):
+        '''Persist a model with version metadata to disk.
+        
+        Parameters
+        ----------
+        model_path : path-like object
+            A path-like object giving the pathname (absolute or relative to
+            the current working directory) of the file to serialize the model
+            to.
+
+        Returns
+        -------
+        None
+        '''
+        with open(model_path, 'wb') as model_file:
+            pickle.dump(self, model_file)
+
+    def deserialize(self, model_path):
+        '''Retrieve a persisted model with version metadata from disk.
+        
+        Parameters
+        ----------
+        model_path : path-like object
+            A path-like object giving the pathname (absolute or relative to
+            the current working directory) of the file to deserialize the
+            model from.
+
+        Returns
+        -------
+        None
+        '''
+        with open(model_path, 'rb') as model_file:
+            model_manager = pickle.load(model_file)
+
+        self.version = model_manager.version
+        self.model = model_manager.model
 
     def fit(self, y, X):
-        '''Fit model to data matrix X and response vector y
+        '''Fit a model to target vector y with respect to data matrix X.
         
         Parameters
         ----------
         y : (M,) array_like
-            Target values
+            Vector of target variable values
         X : (M,N) array_like
-            Training data
+            Data matrix of input variable values
 
         Returns
         -------
@@ -27,34 +95,16 @@ class ModelHandler:
         pass  # Finalized training code
 
     def predict(self, X):
-        '''Predict values y for samples X 
+        '''Predict values for y with respect to data matrix X.
         
         Parameters
         ----------
         X : (M,N) array_like
-            Samples
+            Data matrix of input variable values
 
         Returns
         -------
         y : (M,) array_like
-            Predicted values
+            Vector of predicted values
         '''
         pass  # Code for prediction
-
-    def serialize(self, model_path):
-        '''Persist the model'''
-        if model_path:
-            model_path = Path(model_path)
-        else:
-            model_path = home_dir('')
-
-        pass
-
-    def deserialize(self, model_path):
-        '''Retrieve a persisted model'''
-        if model_path:
-            model_path = Path(model_path)
-        else:
-            model_path = home_dir('')
-
-        pass
